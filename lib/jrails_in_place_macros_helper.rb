7320e369239182ff59509c696e7c589cb9dfa5ad
Kyle Bolton <kbolton@kyle-boltons-imac.local>
2009-10-05 16:45:45 -0400

    fix select option (cherry picked from commit 7fcdc6f8f6ddfd15f28c8e3c024b06ca0bd4ff59)
    Conflicts:
    
    	lib/jrails_in_place_macros_helper.rb (fixed)

--- lib/jrails_in_place_macros_helper.rb
+++ lib/jrails_in_place_macros_helper.rb
@@ -45,7 +45,7 @@ module InPlaceMacrosHelper
     end
     js_options['field_type'] = "'" + options[:field_type] + "'" if options[:field_type]
     js_options['select_options'] = "'" + (options[:select_options].is_a?(Array)?
-      options[:select_options].join(',') : options[:select_options]) + "'" if options[:field_type].to_s == 'select'
+      options[:select_options].join(',') : options[:select_options]) + "'" if js_options[:field_type].to_s == 'select'
     js_options['textarea_cols'] = (options[:textarea_cols] || 25).to_i if options[:field_type].to_s == 'textarea'
     js_options['textarea_rows'] = (options[:textarea_rows] || 10).to_i if options[:field_type].to_s == 'textarea'
     js_options['datepicker'] = "'" + options[:datepicker].to_s + "'" if options[:datepicker]
